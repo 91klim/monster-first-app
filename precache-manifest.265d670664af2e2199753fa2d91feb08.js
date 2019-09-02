ca3aa82c6e8cb9350c736146d11d6e6fe307e8f7
cwp468 <cwp468@motorolasolutions.com>
2019-09-02 19:24:08 +0200

    Updates

---
+++ precache-manifest.265d670664af2e2199753fa2d91feb08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c9aa65ef199cb5178f6dd82847639a",
+    "url": "/monster-first-app/index.html"
+  },
+  {
+    "revision": "768f9a04329e2b7f6881",
+    "url": "/monster-first-app/static/css/main.522f3421.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monster-first-app/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "768f9a04329e2b7f6881",
+    "url": "/monster-first-app/static/js/main.420adbb2.chunk.js"
+  },
+  {
+    "revision": "9b921e1d1546092d5833",
+    "url": "/monster-first-app/static/js/runtime~main.f2ded6e4.js"
+  }
+]);
